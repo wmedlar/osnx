83080fb098f07593852447b67ddf7bf2b2d56a3e
Will Medlar <will.medlar@gmail.com>
2020-08-05 23:30:09 -0500

    Handle mac addresses with leading zeroes trimmed and check for basic validity

--- osnx.sh
+++ osnx.sh
@@ -132,17 +132,30 @@ osnxftp() {
 }
 
 osnxip() {
-    if [ -z "$1" ]; then
-        set -- "$(osnx conf get mac)"
+    local mac="$1"
+
+    if [ -z "$mac" ]; then
+        mac="$(osnx conf get mac)"
     fi
 
-    if [ -z "$1" ]; then
+    if [ -z "$mac" ]; then
         stderr 'please set your mac address with "osnx conf set mac <yo:ur:ma:c:ad:dr:es:s>"'
         return 1
     fi
 
-    # switch settings list the mac in XX-XX-[...] format, convert this to xx:xx:[...]
-    mac="$(echo "$1" | tr '-' ':' | tr '[:upper:]' '[:lower:]')"
+
+    # switch settings list the mac in AB-0X-[...] format, convert this to ab:x:[...]
+    mac="$(echo "$mac" | tr -- '-[:upper:]' ':[:lower:]' | sed -E 's/0([a-f0-9])/\1/g')"
+
+    # minimum mac size, with leading zeroes stripped out, would be 11 (e.g., 1:2:3:4:5:6)
+    # maximum mac size, with doublets and leading zeroes, would be 17 (e.g., a1:b2:c3:d4:e5:f6)
+    # anything outside of this range is either mistyped or the sign of a bug in "osnx conf get"
+    if [ "${#mac}" -lt 11 ] || [ "${#mac}" -gt 17 ]; then
+        stderrf 'mac address %s does not appear to be a valid mac address\n' "$mac"
+        stderr  'please confirm your mac address is set correctly by checking'
+        stderr  'System Settings > Internet > Connection Status > System MAC Address'
+    fi
+
     ip="$(ipfrommac "$mac")"
 
     # switch will not always be present in arp cache
@@ -156,8 +169,7 @@ osnxip() {
 
     if [ -z "$ip" ]; then
         stderrf "no ip found for mac address %s\n\n" "$1"
-        stderr  "confirm your system is online and your mac address is set correctly by checking"
-        stderr  "System Settings > Internet > Connection Status > System MAC Address"
+        stderr  "please confirm your system is online"
         return 1
     fi
 
